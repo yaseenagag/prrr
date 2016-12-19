ff81ce94c4af6282585a19fdb9e55d800753f650
Jared Grippe <jared@deadlyicon.com>
2016-12-19 13:50:43 -0800

    added Icon

---
+++ browser/components/Icon.js
@@ -0,0 +1,16 @@
+import './Icon.sass'
+import React, { Component } from 'react'
+
+const Sizes = ['fa-sm','','fa-lg','fa-2x','fa-3x','fa-4x','fa-5x']
+
+export default function Icon(props) {
+  let size = Number(props.size)
+  if (isNaN(size)) size = 1
+  const iProps = Object.assign({}, props)
+  delete iProps.size
+  delete iProps.type
+  iProps.className = `Icon fa fa-${props.type} ${Sizes[size]} ${props.className||''}`
+  return <i {...iProps}>{props.children}</i>
+}
+
+
