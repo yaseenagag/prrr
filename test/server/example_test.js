1f0d80ad2ec4658bce0967a7948b2122c91aa274
Jared Grippe <jared@deadlyicon.com>
2017-01-04 16:25:59 -0800

    setup test environment

--- test/server/example_test.js
+++
@@ -1,8 +0,0 @@
-describe('example server test', () => {
-  it('should test requests to the server', () => {
-    return request('get', '/hello-world').then(response => {
-      expect(response).to.have.status(200)
-      expect(response.text).to.eql('Hello World')
-    })
-  })
-})
