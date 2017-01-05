8677dba0cceec711acee2ba0e3ce8d2a39615228
Jared Grippe <jared@deadlyicon.com>
2017-01-04 16:25:59 -0800

    setup test environment

--- server/authentication.js
+++ server/authentication.js
@@ -3,14 +3,30 @@ import passport from 'passport'
 import Queries from './queries'
 import Commands from './commands'
 
+let GITHUB_CLIENT_ID
+let GITHUB_CLIENT_SECRET
+let GITHUB_CALLBACK
+
+if (process.env.NODE_ENV === 'development'){
+  GITHUB_CLIENT_ID     = process.env.GITHUB_CLIENT_ID
+  GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
+  GITHUB_CALLBACK      = process.env.GITHUB_CALLBACK
+}
+if (process.env.NODE_ENV === 'test'){
+  GITHUB_CLIENT_ID     = 'FAKE_GITHUB_CLIENT_ID'
+  GITHUB_CLIENT_SECRET = 'FAKE_GITHUB_CLIENT_SECRET'
+  GITHUB_CALLBACK      = 'http://example.com/oauth_callback'
+}
+
 const router = new express.Router()
 
 const GitHubStrategy = require('passport-github').Strategy
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.GITHUB_CALLBACK,
+passport.use(
+  new GitHubStrategy({
+    clientID:     GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL:  GITHUB_CALLBACK,
     scope: ['user:email', 'repo'],
   },
   function(accessToken, refreshToken, profile, cb) {
--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,20 @@
-console.log('test/setup.js')
+import sinon from 'sinon'
+import knex from '../server/knex'
+import Queries from '../server/queries'
+import Commands from '../server/commands'
+
+beforeEach(done => {
+  knex.migrate.latest()
+    .then(_ =>
+      Promise.all([
+        knex.truncate('users'),
+        knex.truncate('pull_request_review_requests'),
+      ])
+    )
+    .then(_ => done())
+})
+
+global.sinon = sinon
+global.knex = knex
+global.Queries = Queries
+global.Commands = Commands
